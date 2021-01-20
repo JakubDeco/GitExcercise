e4873bc6fdba76dbd9360d69365a5cb1e33813e3
JakubDeco <jakub.deco@kosickaakademia.sk>
2021-01-20 10:56:37 +0100

    WithRegex.isDigit added description

--- src/sk/kosickaakademia/deco/regexes/WithRegex.java
+++ src/sk/kosickaakademia/deco/regexes/WithRegex.java
@@ -6,7 +6,7 @@ public class WithRegex {
     public static void main(String[] args) {
         System.out.println(isDigit("gs8"));
     }
-
+    /**method compares parameter String with pattern*/
     public static boolean isDigit(String s) {
         return Pattern.matches("\\d",s);
 

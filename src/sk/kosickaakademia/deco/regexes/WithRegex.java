88ded33465c8b84d2a439ee069417131a7fd6ece
JakubDeco <jakub.deco@kosickaakademia.sk>
2021-01-20 10:48:18 +0100

    update WithRegex.main: calling isDigit

--- src/sk/kosickaakademia/deco/regexes/WithRegex.java
+++ src/sk/kosickaakademia/deco/regexes/WithRegex.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class WithRegex {
     public static void main(String[] args) {
-
+        System.out.println(isDigit("gs8"));
     }
 
     public static boolean isDigit(String s) {
